93168576eef8a69aa957b9dbf335e4842bede17f
Pik1607 <om_pikun@ros-stroi.com>
2024-10-11 23:43:18 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+Number_of_completed=12
+Number_of_hours=1.5
+Course_name='Python'
+Time_for_one_task=(Number_of_hours/Number_of_completed)
+print("Курс:",Course_name,",","всего задач:",Number_of_completed,",","среднее время выполнения",Time_for_one_task,"часа.")
